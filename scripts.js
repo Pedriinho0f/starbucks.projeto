e7bb882f8eb8d847cdfb0966425d7d7b51cc6482
Pedriinho <157435851+Pedriinho0f@users.noreply.github.com>
2025-05-19 23:09:03 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+let circulo = document.querySelector (".circulo")
+let imagem = document.querySelector (".starbucks")
+
+
+
+function trocarcor(cor){
+   circulo.style.background = cor
+
+   }
+
+   function trocaimagem(img)  {
+      imagem.src = img
+   }
